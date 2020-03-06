041aa9d540c62d048349e9238149859211605f5b
Alexandre Roulois <alexandre.roulois@linguist.univ-paris-diderot.fr>
2020-03-06 16:05:21 +0100

    Exercices and solutions

---
+++ 4.text-processing/scripts/melville-moby_dick.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import nltk, unicodedata, string
+from nltk.corpus import gutenberg, stopwords
+
+def occurrences(words):
+    """Builds up a dictionary of words and the count of their
+    occurrences. Ex : {'word': 3}
+    
+    Keyword argument:
+    words -- list of tokens
+    """
+    result = {}
+    for word in words:
+        if word not in stopwords.words('english') + list(string.punctuation):
+            result.update({
+                word: result.get(word, 0) + 1
+            })
+    return result
+
+# List of words in Moby Dick
+words = gutenberg.words('melville-moby_dick.txt')
+
+# Builds up a dictionary of occurrences
+occurrences = occurrences(words)
+
+# Transforms the dict into a sortable object
+results = list(occurrences.items())
+
+# Lowercase before sorting
+results.sort(key=lambda this:this[0].lower())
+
+# Sorting by decreasing number of occurrences
+results.sort(key=lambda this:this[1], reverse=True)
